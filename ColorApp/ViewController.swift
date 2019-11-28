ccd039eae9c7ebd370382e91fd6784633363344c
aviciida <ryoiida9507@gmail.com>
2019-11-28 22:16:08 +0900

    スタート

--- ColorApp/ViewController.swift
+++ ColorApp/ViewController.swift
@@ -11,9 +11,6 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var redValue: UILabel!
-    
-    
     @IBOutlet weak var colorView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
