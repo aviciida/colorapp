283362936cd93d7ec86d9df8f874eac1e0baac8d
aviciida <ryoiida9507@gmail.com>
2019-11-28 22:15:05 +0900

    :sparkles: First commit

---
+++ ColorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorApp
+//
+//  Created by 飯田諒 on 2019/11/28.
+//  Copyright © 2019 RyoIida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var redValue: UILabel!
+    
+    
+    @IBOutlet weak var colorView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        colorView.backgroundColor = UIColor.black
+    }
+
+
+}
+
